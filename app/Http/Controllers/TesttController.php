8f95cf7c544ea209600e8f86e4a6dfed047c1f7b
coumarin_geraniol <semahatartikova@gmail.com>
2023-01-30 10:00:04 +0300

    test add

--- app/Http/Controllers/TesttController.php
+++ app/Http/Controllers/TesttController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TestController extends Controller
+class TesttController extends Controller
 {
     public function index()
     {
